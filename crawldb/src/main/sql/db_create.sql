ed0b47316a9699e9e66bfcbbaa2a3f42dbda9db9
Ludovic PENET <ludovic@penet.org>
2015-01-23 14:58:10 +0100

    Removing FK from word_pages to word (which induced a massive performance hit).

--- crawldb/src/main/sql/db_create.sql
+++ crawldb/src/main/sql/db_create.sql
@@ -1,8 +1,20 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema crawl
+-- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `crawl` ;
+
+-- -----------------------------------------------------
+-- Schema crawl
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `crawl` DEFAULT CHARACTER SET latin1 ;
 USE `crawl` ;
 
@@ -11,12 +23,12 @@ USE `crawl` ;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `crawl`.`runs` ;
 
-CREATE  TABLE IF NOT EXISTS `crawl`.`runs` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `seed` VARCHAR(255) NOT NULL ,
-  `start` TIMESTAMP NULL DEFAULT NULL ,
-  `end` TIMESTAMP NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
+CREATE TABLE IF NOT EXISTS `crawl`.`runs` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `seed` VARCHAR(255) NOT NULL,
+  `start` TIMESTAMP NULL DEFAULT NULL,
+  `end` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 53
 DEFAULT CHARACTER SET = utf8;
@@ -27,25 +39,28 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `crawl`.`pages` ;
 
-CREATE  TABLE IF NOT EXISTS `crawl`.`pages` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `runid` INT(11) NOT NULL ,
-  `url` VARCHAR(2048) NOT NULL ,
-  `status` INT(11) NULL DEFAULT NULL ,
-  `title` VARCHAR(2048) NULL DEFAULT NULL ,
-  `text` MEDIUMTEXT NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`, `runid`) ,
-  INDEX `fk_pages_runs_idx` (`runid` ASC) ,
-  INDEX `idx_pages_url` (`url`(255) ASC) ,
-  INDEX `idx_pages_status` (`status` ASC) ,
+CREATE TABLE IF NOT EXISTS `crawl`.`pages` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `runid` INT(11) NOT NULL,
+  `url` VARCHAR(2048) NOT NULL,
+  `status` INT(11) NULL DEFAULT NULL,
+  `title` VARCHAR(2048) NULL DEFAULT NULL,
+  `text` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `runid`),
   CONSTRAINT `fk_pages_runs`
-    FOREIGN KEY (`runid` )
-    REFERENCES `crawl`.`runs` (`id` )
+    FOREIGN KEY (`runid`)
+    REFERENCES `crawl`.`runs` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 AUTO_INCREMENT = 91023
 DEFAULT CHARACTER SET = utf8;
+
+CREATE INDEX `fk_pages_runs_idx` ON `crawl`.`pages` (`runid` ASC);
+
+CREATE INDEX `idx_pages_url` ON `crawl`.`pages` (`url`(255) ASC);
+
+CREATE INDEX `idx_pages_status` ON `crawl`.`pages` (`status` ASC);
 
 
 -- -----------------------------------------------------
@@ -53,28 +68,31 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `crawl`.`links` ;
 
-CREATE  TABLE IF NOT EXISTS `crawl`.`links` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `page_from` INT(11) NOT NULL ,
-  `page_to` INT(11) NOT NULL ,
-  `text` VARCHAR(2048) NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_page_from_idx` (`page_from` ASC) ,
-  INDEX `fk_page_to_idx` (`page_to` ASC) ,
-  INDEX `text_idx` (`text`(767) ASC) ,
+CREATE TABLE IF NOT EXISTS `crawl`.`links` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `page_from` INT(11) NOT NULL,
+  `page_to` INT(11) NOT NULL,
+  `text` VARCHAR(2048) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_page_from`
-    FOREIGN KEY (`page_from` )
-    REFERENCES `crawl`.`pages` (`id` )
+    FOREIGN KEY (`page_from`)
+    REFERENCES `crawl`.`pages` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_page_to`
-    FOREIGN KEY (`page_to` )
-    REFERENCES `crawl`.`pages` (`id` )
+    FOREIGN KEY (`page_to`)
+    REFERENCES `crawl`.`pages` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 AUTO_INCREMENT = 123043
 DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_page_from_idx` ON `crawl`.`links` (`page_from` ASC);
+
+CREATE INDEX `fk_page_to_idx` ON `crawl`.`links` (`page_to` ASC);
+
+CREATE INDEX `text_idx` ON `crawl`.`links` (`text`(767) ASC);
 
 
 -- -----------------------------------------------------
@@ -82,42 +100,22 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `crawl`.`map_reduce_jobs` ;
 
-CREATE  TABLE IF NOT EXISTS `crawl`.`map_reduce_jobs` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `runid` INT(11) NULL DEFAULT NULL ,
-  `appenginemrid` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_mrj_runs` (`runid` ASC) ,
-  INDEX `idx_appenginemrid` (`appenginemrid` ASC) ,
+CREATE TABLE IF NOT EXISTS `crawl`.`map_reduce_jobs` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `runid` INT(11) NULL DEFAULT NULL,
+  `appenginemrid` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_mrj_runs`
-    FOREIGN KEY (`runid` )
-    REFERENCES `crawl`.`runs` (`id` )
+    FOREIGN KEY (`runid`)
+    REFERENCES `crawl`.`runs` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
+CREATE INDEX `fk_mrj_runs` ON `crawl`.`map_reduce_jobs` (`runid` ASC);
 
--- -----------------------------------------------------
--- Table `crawl`.`words`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `crawl`.`words` ;
-
-CREATE  TABLE IF NOT EXISTS `crawl`.`words` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `runid` INT(11) NULL DEFAULT NULL ,
-  `word` VARCHAR(256) NULL DEFAULT NULL ,
-  `occurrences` INT(11) NULL DEFAULT '0' ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `run_word_idx` (`runid` ASC, `word` ASC) ,
-  INDEX `fk_words_runs_idx` (`runid` ASC) ,
-  CONSTRAINT `fk_words_runs`
-    FOREIGN KEY (`runid` )
-    REFERENCES `crawl`.`runs` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+CREATE INDEX `idx_appenginemrid` ON `crawl`.`map_reduce_jobs` (`appenginemrid` ASC);
 
 
 -- -----------------------------------------------------
@@ -125,26 +123,46 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `crawl`.`word_pages` ;
 
-CREATE  TABLE IF NOT EXISTS `crawl`.`word_pages` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `word` INT(11) NULL DEFAULT NULL ,
-  `page` INT(11) NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_word_pages_words_idx` (`word` ASC) ,
-  INDEX `fk_word_pages_pages_idx` (`page` ASC) ,
-  CONSTRAINT `fk_word_pages_words`
-    FOREIGN KEY (`word` )
-    REFERENCES `crawl`.`words` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+CREATE TABLE IF NOT EXISTS `crawl`.`word_pages` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `word` VARCHAR(256) NULL DEFAULT NULL,
+  `page` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
   CONSTRAINT `fk_word_pages_pages`
-    FOREIGN KEY (`page` )
-    REFERENCES `crawl`.`pages` (`id` )
+    FOREIGN KEY (`page`)
+    REFERENCES `crawl`.`pages` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
+CREATE INDEX `fk_word_pages_words_idx` ON `crawl`.`word_pages` (`word` ASC);
+
+CREATE INDEX `fk_word_pages_pages_idx` ON `crawl`.`word_pages` (`page` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `crawl`.`words`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `crawl`.`words` ;
+
+CREATE TABLE IF NOT EXISTS `crawl`.`words` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `runid` INT(11) NULL DEFAULT NULL,
+  `word` VARCHAR(256) NULL DEFAULT NULL,
+  `occurrences` INT(11) NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_words_runs`
+    FOREIGN KEY (`runid`)
+    REFERENCES `crawl`.`runs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE UNIQUE INDEX `run_word_idx` ON `crawl`.`words` (`runid` ASC, `word` ASC);
+
+CREATE INDEX `fk_words_runs_idx` ON `crawl`.`words` (`runid` ASC);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
